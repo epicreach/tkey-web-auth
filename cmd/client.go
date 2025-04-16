4a8ba3d6668c9f92ae09fa6791622daca56dcf2b
epicreach <luqas.lundahl4817@gmail.com>
2025-04-16 22:20:20 +0200

    removed mode selector feature

--- cmd/client.go
+++ cmd/client.go
@@ -6,9 +6,7 @@ package main
 import (
 	"chalmers/tkey-group22/client/internal/auth"
 	. "chalmers/tkey-group22/client/internal/structs"
-	"chalmers/tkey-group22/client/internal/util"
 	"encoding/json"
-	"flag"
 	"fmt"
 	"io"
 	"net/http"
@@ -16,41 +14,8 @@ import (
 )
 
 func main() {
-	// Define a flag to choose between cmd-client and web-client
-	// Run web-client by default
-	mode := flag.String("mode", "web", "Choose the mode to run: cmd or web")
-	flag.Parse()
-
-	// Start the appropriate client based on the flag value
-	switch *mode {
-	case "cmd":
-		fmt.Println("Starting command-line client...")
-		startCmdClient()
-	default:
-		fmt.Println("Starting web client...")
-		startWebClient()
-	}
-}
-
-func startCmdClient() {
-	// Gets mode from user inputs and runs selected mode. Loops until program is told to exit.
-	for {
-		mode := util.SelectMode()
-
-		switch mode {
-		case 1:
-			// Perform register
-			util.CallRegister()
-		case 2:
-			// Perform login
-			util.CallLogin()
-		case 3:
-			// Stop program
-			return
-		default:
-			fmt.Println("Invalid choice, please try again.")
-		}
-	}
+	fmt.Println("Starting web client...")
+	startWebClient()
 }
 
 // Starts http listeners for the web client to use
